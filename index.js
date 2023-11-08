e53d81b4b50a0dd5d1bf677c174c93ba1c9d5ea3
Shariar <shariarnafis86@gmail.com>
2023-11-08 17:17:18 +0600

    delete a job

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -31,23 +37,51 @@ async function run() {
 
     const allJobsCollection = client.db('stackJobsDB').collection('allJobs')
 
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: false,
+          // sameSite: 'none'
+        })
+        .send({ success: true })
+    })
 
-    app.get('/allJobs', async(req, res)=>{
+
+    //job related api
+    app.get('/allJobs', async (req, res) => {
       const cursor = allJobsCollection.find()
       const result = await cursor.toArray()
       res.send(result)
     })
-    app.get('/myJobs', async(req, res)=>{
-      const cursor = allJobsCollection.find()
-      const result = await cursor.toArray()
+
+    app.get('/myJobs', async (req, res) => {
+      console.log(req.query.email)
+      
+      let query = {};
+      if (req.query?.email) {
+        query = { userEmail: req.query.email }
+      }
+      const result = await allJobsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.delete('/allJobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await allJobsCollection.deleteOne(query)
       res.send(result)
     })
 
 
-    app.post('/addAJob', async(req,res)=>{
-        const newJob = req.body;
-        const result = await allJobsCollection.insertOne(newJob)
-        res.send(result)
+    app.post('/addAJob', async (req, res) => {
+      const newJob = req.body;
+      const result = await allJobsCollection.insertOne(newJob)
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -62,10 +96,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('stack jobs is running')
+app.get('/', (req, res) => {
+  res.send('stack jobs is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 
 })
